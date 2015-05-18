eb9038142270c9cdc421b44ea36ccff5686e1259
zxc2694 <zxc2694zxc2694@gmail.com>
2015-05-18 16:58:50 +0800

    Support value of "int" (data type) to show the LCD screen.

--- Firmware/LCD_with_UART/main.c
+++ Firmware/LCD_with_UART/main.c
@@ -1,11 +1,11 @@
 #include "stm32f4xx.h" 
 #include "tm_stm32f4_ili9341.h"
 #include "tm_stm32f4_fonts.h"
- #include "tm_stm32f4_disco.h"
+ //#include "tm_stm32f4_disco.h"
 #include "tm_stm32f4_sdram.h"
 #include "tm_stm32f4_delay.h"
 #include "tm_stm32f4_usart.h"
-#include "tm_stm32f4_usart_dma.h"
+//#include "tm_stm32f4_usart_dma.h"
 #include "string.h"
 
 int main(void) {
@@ -18,7 +18,19 @@ int main(void) {
 	TM_ILI9341_Fill(ILI9341_COLOR_MAGENTA);
 	TM_USART_Init(USART3, TM_USART_PinsPack_1, 9600); // TX= PB10 (White line); RX= PB11 (Green line)
 	TM_USART_Puts(USART3, "Test the LCD with UART\n\n\r\r");
-	TM_ILI9341_Puts(30, 10, "Test the LCD with UART", &TM_Font_11x18, ILI9341_COLOR_BLACK, ILI9341_COLOR_BLUE2);
+	TM_ILI9341_Puts(5, 10, "Test the LCD with UART", &TM_Font_11x18, ILI9341_COLOR_BLACK, ILI9341_COLOR_BLUE2);
+
+	/* test ASCII for LCD's display */
+	int a=72,a1,a2;  // Input a value 72 to show the screen to test int(資料型態)
+
+    a1=a*0.1; // 從int轉換為轉char
+    a2=a%10;
+
+    a1=a1+48; // 從char轉換為ascii
+    a2=a2+48;
+
+    TM_ILI9341_Putc(265, 10, a1, &TM_Font_11x18, ILI9341_COLOR_BLACK, ILI9341_COLOR_BLUE2);
+    TM_ILI9341_Putc(275, 10, a2, &TM_Font_11x18, ILI9341_COLOR_BLACK, ILI9341_COLOR_BLUE2);
 
 	while (1) {
 		c = TM_USART_Getc(USART3);
